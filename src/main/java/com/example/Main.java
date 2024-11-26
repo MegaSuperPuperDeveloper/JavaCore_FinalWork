81c6ffbee9c4dd403bc605193549d170d6700fdc
kruto <krutoi.chuvak1918@gmail.com>
2024-11-26 22:41:07 +0300

    JavaCore_FinalWork

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -11,7 +11,7 @@ public class Main {
             System.out.println("Вы выбрали первый вариант");
             Scanner scanner = new Scanner(System.in);
             String sourceDirectory = scanner.next();
-            Task1.createBackup(sourceDirectory);
+            Task1.createBackup(sourceDirectory); // РЕЗЕРВНАЯ КОПИЯ СОХРАНЯЕТСЯ В ТУ ЖЕ ПАПКУ, КОТОРАЯ КОПИРОВАЛАСЬ!
         } else if (ans == 2 ) {
             System.out.println("Вы выбрали второй вариант");
             Task2.newGame();
