b91effb7d50d23cb3ca87fa7b647fca6039b2e75
kruto <krutoi.chuvak1918@gmail.com>
2024-11-26 22:40:18 +0300

    JavaCore_FinalWork

--- src/main/java/com/example/Task1.java
+++ src/main/java/com/example/Task1.java
@@ -6,26 +6,25 @@ import java.nio.file.*;
 public class Task1 {
 
     public static void createBackup(String sourceDirectoryPath) {
-        // Путь к папке для резервного копирования
         Path sourceDirPath = Paths.get(sourceDirectoryPath);
         Path backupDirPath = sourceDirPath.resolve("backup");
 
         try {
-            // Создаем папку ./backup, если ее нет
+
             if (!Files.exists(backupDirPath)) {
+
                 Files.createDirectory(backupDirPath);
                 System.out.println("Папка 'backup' создана");
+
             }
 
-            // Проходим по всем файлам в исходной директории (без поддиректорий)
             try (DirectoryStream<Path> stream = Files.newDirectoryStream(sourceDirPath, path -> Files.isRegularFile(path))) {
                 for (Path filePath : stream) {
-                    // Формируем путь для файла в папке backup
-                    Path destinationPath = backupDirPath.resolve(filePath.getFileName());
 
-                    // Копируем файл
+                    Path destinationPath = backupDirPath.resolve(filePath.getFileName());
                     Files.copy(filePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
                     System.out.println("Файл скопирован: " + filePath.getFileName());
+
                 }
             } catch (IOException e) {
                 System.out.println("Ошибка при перечислении файлов: " + e.getMessage());
